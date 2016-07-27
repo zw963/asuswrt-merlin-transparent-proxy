1dc0296c60001191cfccf550989e537cc9ec4cf6
Billy.Zheng <zw963@163.com>
2016-07-27 19:17:31 +0800

    Fixed: iptables.
    asuswrt-merlin will clean iptables NAT table `shadowsocks' in some case,
    this change ensure about iptables will revalidated when cleanup.

--- route/opt/etc/iptables.sh
+++ route/opt/etc/iptables.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-if /opt/sbin/ipset -N FREEWEB iphash; then
-    if /opt/sbin/iptables -t nat -N SHADOWSOCKS; then
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+# asuswrt-merlin 有可能清空 SHADOWSOCKS 表。
+# IF 条件确保只有在这个表被清空后，才重新执行 iptables.
 
-        # 如果有很多服务器，并且所有目标服务器都是同样的端口, 用法如下
-        # /opt/sbin/iptables -t nat -A SHADOWSOCKS --dport 22334 -j RETURN
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
+if /opt/sbin/iptables -t nat -N SHADOWSOCKS; then
+    /opt/sbin/ipset -N FREEWEB iphash
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
 
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -m set --match-set FREEWEB dst -j RETURN
+    # 如果有很多服务器，并且所有目标服务器都是同样的端口, 用法如下
+    # /opt/sbin/iptables -t nat -A SHADOWSOCKS --dport 22334 -j RETURN
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
 
-        /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -m set --match-set FREEWEB dst -j RETURN
 
-        /opt/sbin/iptables -t nat -I PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
-        /opt/sbin/iptables -t nat -I OUTPUT -p tcp -j SHADOWSOCKS
-        # /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
-    fi
+    /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
+
+    /opt/sbin/iptables -t nat -I PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
+    /opt/sbin/iptables -t nat -I OUTPUT -p tcp -j SHADOWSOCKS
+    # /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
 fi
 
 # 查看 ipset
