61592999906456d5a8c97b609716579e1ebef679
Billy.Zheng <zw963@163.com>
2017-02-18 14:08:21 +0800

    Fix ipset protocal version 6 support for asus AC87U

--- route/opt/etc/iptables.sh
+++ route/opt/etc/iptables.sh
@@ -3,29 +3,48 @@
 # asuswrt-merlin 有可能清空 SHADOWSOCKS 表。
 # IF 条件确保只有在这个表被清空后，才重新执行 iptables.
 
-if /opt/sbin/iptables -t nat -N SHADOWSOCKS; then
-    /opt/sbin/ipset -N FREEWEB iphash
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+# 添加 AC87U 的 ipset protocal version 6 的 iptables/ipset 支持.
+# See Following issue for detail:
+# https://github.com/zw963/asuswrt-merlin-transparent-proxy/issues/4
+# https://github.com/RMerl/asuswrt-merlin/issues/1062
+ipset_protocal_version=$(ipset version |grep 'version:' |grep -o '[0-9]$')
+
+if [ "$ipset_protocal_version" == 6 ]; then
+    ipset='/usr/sbin/ipset'
+    iptables='/usr/sbin/iptables'
+    instmod ip_set
+    instmod ip_set_hash_net
+    instmod ip_set_hash_ip
+    instmod xt_set
+else
+    ipset='/opt/sbin/ipset'
+    iptables='/opt/sbin/iptables'
+    instmod ip_set ip_set_nethash ip_set_iphash ipt_set
+fi
+
+if $iptables -t nat -N SHADOWSOCKS; then
+    $ipset -N FREEWEB iphash
+    $iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
 
     # 如果有很多服务器，并且所有目标服务器都是同样的端口, 用法如下
-    # /opt/sbin/iptables -t nat -A SHADOWSOCKS --dport 22334 -j RETURN
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
+    # $iptables -t nat -A SHADOWSOCKS --dport 22334 -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
 
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -m set --match-set FREEWEB dst -j RETURN
+    $iptables -t nat -A SHADOWSOCKS -m set --match-set FREEWEB dst -j RETURN
 
-    /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
+    $iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
 
-    /opt/sbin/iptables -t nat -I PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
-    # /opt/sbin/iptables -t nat -I OUTPUT -p tcp -j SHADOWSOCKS
-    # /opt/sbin/iptables -t nat -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
+    $iptables -t nat -I PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
+    # $iptables -t nat -I OUTPUT -p tcp -j SHADOWSOCKS
+    # $iptables -t nat -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
 fi
 
 # 查看 ipset
-/opt/sbin/ipset -L FREEWEB
+$ipset -L FREEWEB
