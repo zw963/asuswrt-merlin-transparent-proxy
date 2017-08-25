6500fadf97b53db96046263053b06fd38ad52ac3
Billy.Zheng <zw963@163.com>
2017-08-25 23:11:03 +0800

    Refactor iptables.sh, Enable udp relay support for ARM.

--- route/opt/etc/iptables.sh
+++ route/opt/etc/iptables.sh
@@ -30,9 +30,18 @@ else
     insmod ipt_set
 fi
 
-# =================== tcp rule =================
+localips='
+0.0.0.0/8
+10.0.0.0/8
+127.0.0.0/8
+169.254.0.0/16
+172.16.0.0/12
+192.168.0.0/16
+224.0.0.0/4
+240.0.0.0/4
+'
 
-ipset -N FREEWEB iphash
+# =================== tcp rule =================
 
 # iptables 默认有四个表: raw, nat, mangle, filter, 每个表都有若干个不同的 chain.
 # 例如: filter 表包含 INPUT, FORWARD, OUTPUT 三个链.
@@ -41,29 +50,27 @@ ipset -N FREEWEB iphash
 # 换成 -I 链名, 则新插入的规则变为第一条规则.
 
 # 插入本地地址直接返回的 rule 到 SHADOWSOCKS chain
-$iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-$iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+
+ipt="$iptables -t nat"
+
+for i in $localips; do
+    $ipt -A SHADOWSOCKS -d "$i" -j RETURN
+done
 
 # 如果访问 VPS 地址, 直接返回.
-$iptables -t nat -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
+$ipt -A SHADOWSOCKS -d SS_SERVER_IP -j RETURN
 
 # 如果访问的域名在 FREEWEB 中, 直接返回.
-$iptables -t nat -A SHADOWSOCKS -p tcp -m set --match-set FREEWEB dst -j RETURN
+$ipt -A SHADOWSOCKS -p tcp -m set --match-set FREEWEB dst -j RETURN
 
 # 如果没有在之前的 rule 中 RETURN, 将执行下面的 rule.
 # 这个 rule 将转发所有的 tcp 请求到 ss-redir 的本地端口.
-$iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
+$ipt -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports SS_LOCAL_PORT
 
 # 在 NAT 的初期阶段 (prerouting 阶段) 将发送到 80, 443 的所有请求, 应用 SHADOWSOCKS chain 中的规则.
-# $iptables -t nat -A PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
-$iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
-# $iptables -t nat -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
+# $ipt -A PREROUTING -p tcp -m multiport --dports 80,443 -j SHADOWSOCKS
+$ipt -I PREROUTING 1 -p tcp -j SHADOWSOCKS
+# $ipt -A SHADOWSOCKS -p tcp --syn -m connlimit --connlimit-above 32 -j RETURN
 
 # ====================== udp rule =======================
 
@@ -72,34 +79,18 @@ if ! modprobe xt_TPROXY; then
     exit
 fi
 
-# 下面的代码因为路由器不支持没跑过, 可能有问题.
-
-$iptables -t mangle -N SHADOWSOCKS
-# iptables -t mangle -N SHADOWSOCKS_MARK
-
-$iptables -t mangle -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
-$iptables -t mangle -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
-
 ip rule add fwmark 0x01/0x01 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
 
-# $iptables -t mangle -A SHADOWSOCKS -p udp -m set --match-set FREEWEB dst -j RETURN
-# $iptables -t mangle -A SHADOWSOCKS -p udp -j MARK --set-mark 1
+ipt="$iptables -t mangle"
 
-# $iptables -t mangle -A PREROUTING -p tcp --dport 80 -j TPROXY \
-    #          --tproxy-mark 0x1/0x1 --on-port 50080
-# # $iptables -t mangle -A SHADOWSOCKS -p udp -j TPROXY --on-port 1082 --tproxy-mark 0x01/0x01
+$ipt -N SHADOWSOCKS
 
-$iptables -t mangle -A SHADOWSOCKS -p udp -m set --match-set FREEWEB dst -j RETURN
-# 因为 tproxy 模块的问题, 下一行代码不工作.
-$iptables -t mangle -A SHADOWSOCKS -p udp -j TPROXY --on-port 1082 --tproxy-mark 0x01/0x01
-# $iptables -t mangle -A SHADOWSOCKS_MARK -p udp -m set --match-set gfwlist dst -j MARK --set-mark 1
-
-$iptables -t mangle -A PREROUTING -p udp -j SHADOWSOCKS
-# $iptables -t mangle -A OUTPUT -j SHADOWSOCKS_MARK
+# 下面基于 udp 的规则, 类似于 tcp,.
+for i in $localips; do
+    $ipt -A SHADOWSOCKS -d "$i" -j RETURN
+done
+$ipt -A SHADOWSOCKS -p udp -m set --match-set FREEWEB dst -j RETURN
+$ipt -A SHADOWSOCKS -p udp -j TPROXY --on-port 1082 --tproxy-mark 0x01/0x01
+# 这里可以 -i interface 来手动指定仅仅路由指定的接口.
+$ipt -I PREROUTING 1 -p udp -j SHADOWSOCKS
