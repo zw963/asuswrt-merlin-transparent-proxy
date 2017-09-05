e1d87844ee52787f3f3ab4b89f3ee0bdb4f026fe
Billy.Zheng <zw963@163.com>
2017-09-05 14:39:06 +0800

    Fix bug.

--- route/opt/etc/iptables.sh
+++ route/opt/etc/iptables.sh
@@ -82,4 +82,4 @@ $ipt -A SHADOWSOCKS -p udp -m set --match-set FREEWEB dst -j RETURN
 $ipt -A SHADOWSOCKS -p udp -j TPROXY --on-port 1082 --tproxy-mark 0x01/0x01
 
 # 应用规则, 注释这行代码, 重启后会让 UDP rules 失效.
-$ipt -I PREROUTING 1 -j SHADOWSOCKS
+$ipt -I PREROUTING 1 -p udp -j SHADOWSOCKS
