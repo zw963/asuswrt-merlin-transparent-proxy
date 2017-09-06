63767752ebc643904be13a0fe0308abc3e1bdaea
Billy.Zheng <zw963@163.com>
2017-09-06 15:53:03 +0800

    Add test info

--- route/opt/etc/iptables.sh
+++ route/opt/etc/iptables.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# 添加 AC87U 的 ipset protocal version 6 的 iptables/ipset 支持.
-# See Following issue for detail:
-# https://github.com/zw963/asuswrt-merlin-transparent-proxy/issues/4
-# https://github.com/RMerl/asuswrt-merlin/issues/1062
-
 # use iptables-restore < /opt/etc/iptables.rules 恢复原始的 iptables
 [ -f /opt/etc/iptables.rules ] || iptables-save > /opt/etc/iptables.rules
 
@@ -65,11 +60,6 @@ $ipt -I PREROUTING 1 -p tcp -j SHADOWSOCKS
 
 # ====================== udp rule =======================
 
-# 貌似下面的 rule 对于 ss-tunnel 是无用的, 经过测试, FREEWEB 相关的 rule 也是无效的.
-
-# 先跳过下面代码的执行
-exit
-
 if ! modprobe xt_TPROXY; then
     echo 'Kernel not support tproxy!'
     exit
@@ -86,6 +76,38 @@ for i in $localips; do
     $ipt -A SHADOWSOCKS -d "$i" -j RETURN
 done
 
+# 这里的 FREEWEB 是在 dnsmasq 里面添加的白名单域名, 也就是国内可以直接访问的域名列表.
+# 我期望的目的是:
+# - 使用单个的 ss-redir 就可以实现透明代理. (无需 ss-tunnel), 好吧, 这是第一步, 先解决这个.
+# - dnsmasq 中有大量的类似于下面的 ipset 规则(全都是国内域名)
+
+# ipset=/0-6.com/FREEWEB
+# ipset=/0-gold.net/FREEWEB
+# ipset=/00.net/FREEWEB
+# ipset=/0000738.com/FREEWEB
+# ipset=/0001688.com/FREEWEB
+# ipset=/000219.com/FREEWEB
+# ipset=/0007.net/FREEWEB
+# ipset=/000dn.com/FREEWEB
+
+# ...
+
+# ipset=/zzz4.com/FREEWEB
+# ipset=/zzzj.com/FREEWEB
+# ipset=/zzzmode.com/FREEWEB
+# ipset=/zzzyb.com/FREEWEB
+# ipset=/zzzyit.com/FREEWEB
+# ipset=/zzyjs.com/FREEWEB
+# ipset=/zzyzphoto.com/FREEWEB
+# ipset=/zzz4.com/FREEWEB
+# ipset=/zzzj.com/FREEWEB
+# ipset=/zzzmode.com/FREE
+#
+# server=/#/8.8.8.8#53   # 这里应该怎么设定? 写 ss-redir(1080)端口, 还是 8.8.8.8(53) ??
+#
+# 我希望可以利用这些规则, 实现国内域名用默认, 国外走 redir 的 udp 接口差 DNS
+
+# 下面是我的实现, 很显然, 目前不工作! 而且这里的 --on-port ???? 到底该是什么端口? 晕了.
 $ipt -A SHADOWSOCKS -p udp -m set --match-set FREEWEB dst -j RETURN
 $ipt -A SHADOWSOCKS -p udp --dport 53 -j TPROXY --on-port 1082 --tproxy-mark 0x01/0x01
 
