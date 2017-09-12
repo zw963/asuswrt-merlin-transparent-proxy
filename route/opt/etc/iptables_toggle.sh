9b6e03fd1eed6227b2d16debbe7fb6b444ea8fe4
Billy.Zheng <zw963@163.com>
2017-09-12 09:27:13 +0800

    Add toggle arg for force disable

--- route/opt/etc/iptables_toggle.sh
+++ route/opt/etc/iptables_toggle.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ -x /opt/etc/iptables.sh ] && [ -f /opt/etc/dnsmasq.d/foreign_domains.conf ]; then
+if ([ -x /opt/etc/iptables.sh ] && [ -f /opt/etc/dnsmasq.d/foreign_domains.conf ]) || [ "$1" ]; then
+    echo '当前开启模式, 正在关闭'
     [ -f /tmp/iptables.rules ] && iptables-restore < /tmp/iptables.rules
 
     ip route flush table 100
     ipset destroy CHINAIPS
     mv /opt/etc/dnsmasq.d/foreign_domains.conf /opt/etc/dnsmasq.d/foreign_domains.bak
 else
+    echo '当前关闭模式，正在开启'
     mv /opt/etc/dnsmasq.d/foreign_domains.bak /opt/etc/dnsmasq.d/foreign_domains.conf
     chmod +x /opt/etc/iptables.sh && /opt/etc/iptables.sh
     /opt/etc/restart_dnsmasq
