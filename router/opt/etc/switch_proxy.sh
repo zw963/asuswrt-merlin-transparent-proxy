93c3e85edb982729468e9286a1569d65752ea61d
Billy.Zheng <zw963@163.com>
2018-11-01 15:03:00 +0800

    Add a new command for switch shadowsocks proxy easy.

---
+++ router/opt/etc/switch_proxy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ROOT=${0%/*}
+
+cd $ROOT
+ss_config_files=$(ls -1t shadowsocks.* |grep -v shadowsocks.json)
+
+echo 'select one number:'
+echo "$ss_config_files" |grep -n '.*'
+
+while read 'selected_number'; do
+    selected_config=$(echo "$ss_config_files" |sed -n "${selected_number}p")
+    if [ -e $selected_config ]; then
+        echo "Using config [0m[33m${selected_config}[0m."
+        ln -sf $selected_config shadowsocks.json
+        ./patch_router
+        exit
+    else
+        echo 'config not exist.'
+    fi
+done
