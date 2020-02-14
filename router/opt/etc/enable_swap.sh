ce8ce73a036dd549fb7ff025ba65a675daf758f6
Billy.Zheng <vil963@gmail.com>
2020-02-14 16:31:09 +0800

    Fixed: when first deploy, get "/opt/etc/toggle_proxy.sh not found" confuse user.

--- router/opt/etc/enable_swap.sh
+++ router/opt/etc/enable_swap.sh
@@ -2,6 +2,9 @@
 
 if [ ! -f /tmp/mnt/sda1/myswap.swp ]; then
     #create a 256MB swap file ("count" is in Kilobytes)
+    echo "This step only need be run once when you first deploy."
+    echo "it's maybe [0m[33mvery slow[0m depend on your USB stick speed, please wait a while."
+
     dd if=/dev/zero of=/tmp/mnt/sda1/myswap.swp bs=1k count=1048576
 
     #set up the swap file
